2c177ad845403148b869a93e058860e9dc94fd0d
ramusompalli <ramusompalli143@gmail.com>
2023-02-06 14:31:47 +0530

    added request permission button

--- src/App.js
+++ src/App.js
@@ -2,24 +2,15 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
+
+  const showNotification = () => {
+    Notification.requestPermission().then((perm) => {
+      console.log(perm)
+    })
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-        <p>added text to check changes</p>
-      </header>
-    </div>
+    <button onClick={showNotification}>Show notification</button>
   );
 }
 
